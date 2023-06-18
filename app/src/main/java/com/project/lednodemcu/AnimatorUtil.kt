22e43e12098ef1f76a08b2c2c38d0155ca16490f
srideep-banerjee <banerjee.srideep@gmail.com>
2023-06-18 15:41:43 +0530

    Added fade in animation for background and text color

--- app/src/main/java/com/project/lednodemcu/AnimatorUtil.kt
+++ app/src/main/java/com/project/lednodemcu/AnimatorUtil.kt
@@ -2,21 +2,35 @@ package com.project.lednodemcu
 
 import android.animation.ArgbEvaluator
 import android.animation.ObjectAnimator
+import android.graphics.drawable.ColorDrawable
+import android.graphics.drawable.TransitionDrawable
 import android.view.View
 import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 
 class AnimatorUtil {
-    companion object{
-        fun animateBackgroundColor(view: View, color1: Int, color2: Int){
-            val colorAnimator = ObjectAnimator.ofObject(
-                view, "backgroundColor", ArgbEvaluator(), color1, color2
+    companion object {
+        fun animateBackgroundColor(
+            view: View,
+            color1: Int,
+            color2: Int,
+            context: AppCompatActivity
+        ) {
+            val colorData = arrayOf(
+                ColorDrawable(ContextCompat.getColor(context.applicationContext, color1)),
+                ColorDrawable(ContextCompat.getColor(context.applicationContext, color2))
             )
-            colorAnimator.duration = 300
-            colorAnimator.start()
+            val trans = TransitionDrawable(colorData)
+            view.background = trans
+            trans.startTransition(300)
         }
-        fun animateTextColor(view: TextView, color1: Int, color2: Int){
+
+        fun animateTextColor(view: TextView, color1: Int, color2: Int, context: AppCompatActivity) {
+            val colorFrom = ContextCompat.getColor(context, color1)
+            val colorTo = ContextCompat.getColor(context, color2)
             val colorAnimator = ObjectAnimator.ofObject(
-                view, "textColor", ArgbEvaluator(), color1, color2
+                view, "textColor", ArgbEvaluator(), colorFrom, colorTo
             )
             colorAnimator.duration = 300
             colorAnimator.start()
--- app/src/main/java/com/project/lednodemcu/MainActivity.kt
+++ app/src/main/java/com/project/lednodemcu/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.project.lednodemcu
 
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.ProgressBar
 import android.widget.TextView
@@ -18,43 +19,84 @@ class MainActivity : AppCompatActivity() {
     private lateinit var buttonText: TextView
     private lateinit var httpClient: OkHttpClient
     private lateinit var loadingCircle: ProgressBar
-    private lateinit var progressedButton: MaterialCardView
+    private lateinit var progressedButton: View
+    private lateinit var offlineScreen: View
+    private lateinit var onlineScreen: View
 
-    private val url = ""
+    private val url = "https://www.google.com"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         buttonText = findViewById(R.id.buttonText)
         msgText = findViewById(R.id.msgText)
-        loadingCircle=findViewById(R.id.loadingCircle)
-        progressedButton=findViewById(R.id.progressedButton)
+        loadingCircle = findViewById(R.id.loadingCircle)
+        progressedButton = findViewById(R.id.progressedButton)
+        offlineScreen = findViewById(R.id.offlineScreen)
+        onlineScreen = findViewById(R.id.onlineScreen)
         httpClient = OkHttpClient()
+        progressedButton.setOnClickListener {
+            connect()
+        }
+        connect()
 
     }
 
     fun connect() {
-        msgText.text="Connecting..."
-        buttonText.text="Connecting..."
-        loadingCircle.visibility=View.VISIBLE
-        animateBackgroundColor(progressedButton,R.color.background,R.color.bluePrimary)
-        animateTextColor(buttonText,R.color.bluePrimary,R.color.background)
+        if (msgText.text.equals("Connecting...")) return
+        msgText.text = "Connecting..."
+        buttonText.text = "Connecting..."
+        loadingCircle.visibility = View.VISIBLE
+        animateBackgroundColor(
+            progressedButton,
+            R.color.background,
+            R.color.bluePrimary,
+            this@MainActivity
+        )
+        animateTextColor(buttonText, R.color.bluePrimary, R.color.background, this@MainActivity)
+
 
         val requestBody = "hello".toRequestBody("plain/text".toMediaType())
-        val request = Request.Builder().url(url).post(requestBody).build()
+        val request = Request.Builder().url(url)./*post(requestBody).*/build()
         httpClient.newCall(request).enqueue(object : Callback {
             override fun onFailure(call: Call, e: IOException) {
                 runOnUiThread {
-                    msgText.text="Device Offline"
-                    buttonText.text="Connect"
-                    loadingCircle.visibility=View.INVISIBLE
-                    animateBackgroundColor(progressedButton,R.color.bluePrimary,R.color.background)
-                    animateTextColor(buttonText,R.color.background,R.color.bluePrimary)
+                    msgText.text = "Device Offline"
+                    buttonText.text = "Connect"
+                    loadingCircle.visibility = View.INVISIBLE
+                    animateBackgroundColor(
+                        progressedButton,
+                        R.color.bluePrimary, R.color.background, this@MainActivity
+                    )
+                    animateTextColor(
+                        buttonText,
+                        R.color.background,
+                        R.color.bluePrimary,
+                        this@MainActivity
+                    )
                 }
             }
 
             override fun onResponse(call: Call, response: Response) {
-
+                runOnUiThread {
+                    if (response.isSuccessful) {
+                        offlineScreen.visibility = View.GONE
+                        onlineScreen.visibility = View.VISIBLE
+                    }
+                    msgText.text = "Device Offline"
+                    buttonText.text = "Connect"
+                    loadingCircle.visibility = View.INVISIBLE
+                    animateBackgroundColor(
+                        progressedButton,
+                        R.color.bluePrimary, R.color.background, this@MainActivity
+                    )
+                    animateTextColor(
+                        buttonText,
+                        R.color.background,
+                        R.color.bluePrimary,
+                        this@MainActivity
+                    )
+                }
             }
         })
     }
